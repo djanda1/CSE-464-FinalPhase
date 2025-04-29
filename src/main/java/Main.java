cf03e342bf356a70cb9344da7c794f0a9a486f1e
djanda1 <djanda2@asu.edu>
2025-04-29 16:00:12 -0700

    Push with maven build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         GraphManager gm = new GraphManager();
         try {
-            gm.parseGraph("\\Users\\dbob1\\IdeaProjects\\CSE-464-2025-djanda2\\CSE464ProjectPhase1\\src\\main\\java\\input.dot");
+            gm.parseGraph("input.dot"); // Use resource file
         } catch (java.lang.Exception e) {
             System.out.println(e.getMessage());
         }
